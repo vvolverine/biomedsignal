628bf537635369ac5e9221d8f37a43bb469b281b
Pheodor Paramonov <pheodor.paramonov@gmail.com>
2013-10-23 15:31:07 +0400

    Neuron Sine

--- FirstScript.py
+++ FirstScript.py
@@ -2,11 +2,45 @@
 # -*- coding: utf-8 -*-
 #
 
-
-
+from ffnet import ffnet, mlgraph
+from numpy import *
+from matplotlib.pyplot import *
 
 def main():
-	print "Hello mathematics method!"
+	conec = mlgraph((1, 4 ,1))
+	net = ffnet(conec)
+	
+	patterns = 16
+	input = [[ k * 2 * pi / patterns] for k in xrange(patterns + 1)]
+	target = [[sin(x[0])] for x in input]
+	
+	print "training"
+	net.train_genetic(input, target, individuals=20, generations=500)
+	print "simple trainig"
+	net.train_tnc(input, target, maxfun = 5000, messages = 1)
+	
+	print "test"
+	output, regression = net.test(input, target, iprint = 2)
+	
+	# draw it
+	points = 128
+	xaxis = [[ k * 2 * pi / patterns] for k in xrange(patterns + 1)]
+	sine = [sin(x) for x in xaxis]
+	cosine = [cos(x) for x in xaxis]
+	netsine = [net([x])[0] for x in xaxis]
+	netcosine = [net.derivative([x])[0][0] for x in xaxis]
+	
+	subplot(211)
+	plot(xaxis, sine, 'b--', xaxis, netsine, 'k-')
+	legend(('sine', 'network output'))
+	grid(True)
+	title('Outputs of trained network.')
+
+	subplot(212)
+	plot(xaxis, cosine, 'b--', xaxis, netcosine, 'k-')
+	legend(('cosine', 'network derivative'))
+	grid(True)
+	show()
 	
 	return 0
 
